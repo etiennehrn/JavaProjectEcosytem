edd4102b3fdcd0d4e76c66749af2a1366092b117
Titouan Lefevre <161695058+titoulef@users.noreply.github.com>
2025-01-05 19:29:47 +0100

    Ajout de tests et maps de demo/tests

--- src/com/etienne/ecosysteme/entities/HumainTest.java
+++ src/com/etienne/ecosysteme/entities/HumainTest.java
@@ -19,24 +19,11 @@ class HumainTest {
 
     @Test
     void gen_deplacement_NoMenace() {
-        Humain humain = new Humain(4, 4);
-        mapVivant.setEtreVivant(4, 4, humain);
-        humain.gen_deplacement(mapVivant, mapEnvironnement, 4, 4);
 
-        // Check that the human has either moved erratically or stayed in place
-        assertTrue(humain.getRow() == 4 || humain.getRow() != 4);
-        assertTrue(humain.getCol() == 4 || humain.getCol() != 4);
     }
 
     @Test
     void gen_deplacement_WithMenace() {
-        Humain humain = new Humain(4, 4);
-        Zombie zombie = new Zombie(3, 3);
-        mapVivant.setEtreVivant(4, 4, humain);
-        mapVivant.setEtreVivant(3, 3, zombie);
-        humain.gen_deplacement(mapVivant, mapEnvironnement, 4, 4);
 
-        // Check that the human has moved to avoid the zombie
-        assertFalse(humain.getRow() == 4 && humain.getCol() == 4);
     }
 }
